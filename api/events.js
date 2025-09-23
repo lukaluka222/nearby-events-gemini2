fbfdfdc1af6fcac726fc9c921f98373e26a1b9d9
lukaluka222 <lootanmelon222@gmail.com>
2025-09-23 21:12:33 +0900

    Update events.js

--- api/events.js
+++ api/events.js
@@ -14,10 +14,15 @@ async function getModel() {
 
 // まずは “情報が載ってる固定URL” に絞る（増やすのは後でOK）
 const SOURCES = [
-  // TODO: 相模原市のイベント詳細ページ等に差し替えると精度UP
-  'https://www.city.sagamihara.kanagawa.jp/', // トップは例。実際はイベント詳細URLにする
-  'https://sagamigawa-fureai.com/',           // 施設サイトのイベント/お知らせページなど
+  "https://www.city.sagamihara.kanagawa.jp/event_calendar.html",
+  "https://www.city.sagamihara.kanagawa.jp/kankou/1026674/1026704/event/event_all/index.html",
+  "https://www.city.sagamihara.kanagawa.jp/kankou/bunka/1022291/1022302.html",
+  "https://sagamiharacitymuseum.jp/event/",
+  "https://sagamiharacitymuseum.jp/eventnews/",
+  "https://sagamigawa-fureai.com/commentary/",
+  // 必要に応じて増やす
 ];
+
 
 export default async function handler(req, res) {
   const url = new URL(req.url, `https://${req.headers.host}`);
